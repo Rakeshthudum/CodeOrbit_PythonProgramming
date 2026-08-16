8cd1a5d01e939ecfe40d269ccfc2a549ed8cf7e2
Rakeshthudum <rakeshthudum6@gmail.com>
2026-08-16 22:54:34 +0530

    Create calculator.py
    Organize Simple Calculator project

---
+++ Simple_Calculator/calculator.py
@@ -0,0 +1,25 @@
+print("===== SIMPLE CALCULATOR =====") 
+try:
+   num1 = float(input("Enter first number: ")) 
+   num2 = float(input("Enter second number: ")) 
+   print("\nChoose Operation") 
+   print("1. Addition (+)") 
+   print("2. Subtraction (-)") 
+   print("3. Multiplication (*)") 
+   print("4. Division (/)") 
+   choice = input("Enter your choice (1-4): ") 
+   if choice == "1": 
+      print("Result =", num1 + num2) 
+   elif choice == "2":
+      print("Result =", num1 - num2) 
+   elif choice == "3": 
+      print("Result =", num1 * num2) 
+   elif choice == "4": 
+      if num2 == 0: 
+         print("Error! Division by zero is not allowed.") 
+      else: 
+         print("Result =", num1 / num2) 
+   else: 
+      print("Invalid Choice!") 
+except ValueError:
+   print("Please enter valid numeric values.")
